6c6567939a2e898818f6b21683520d417a291035
lamarireal <170219a7@gmail.com>
2024-01-30 09:59:59 +0100

    actualizado info en jornadas

--- js/Jornadas.js
+++ js/Jornadas.js
@@ -1,58 +1,64 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Получаем параметр 'temporada' из URL
+    // Получаем параметр 'Jornadas' из URL
     const params = new URLSearchParams(window.location.search);
-    const temporada = params.get('Jornadas');
-  
+
+    const Jornada = params.get('Jornadas').replace('Jornadas', '');
+
     // Загружаем XML и обновляем данные для выбранного сезона
     fetch('/XMLyXSD/XMLJornadas.xml')
-      .then(response => response.text())
-      .then(xmlString => {
-        const parser = new DOMParser();
-        const xmlDoc = parser.parseFromString(xmlString, 'application/xml');
-  
-        // Вызываем функцию для обновления информации о туре
-        updateJornadaInfo(xmlDoc, temporada);
-      });
+        .then(response => response.text())
+        .then(xmlString => {
+            const parser = new DOMParser();
+            const xmlDoc = parser.parseFromString(xmlString, 'application/xml');
+
+            // Вызываем функцию для обновления информации о туре
+            updateJornadaInfo(xmlDoc, Jornada);
+        });
 });
 
-function updateJornadaInfo(xmlDoc, temporada) {
-    console.log(temporada);
-    const jornadaElement = xmlDoc.querySelector(`Temporada${temporada}`);
+function updateJornadaInfo(xmlDoc, Jornada) {
+    console.log(Jornada);
+    const jornadaElement = xmlDoc.querySelector(`Jornada${Jornada}`);
     console.log(jornadaElement);
-
     if (jornadaElement) {
-        const jornadaEspecificaElement = jornadaElement.querySelector(`Jornada1`);
-        const partidos = jornadaEspecificaElement.querySelectorAll('Partido');
-        console.log(jornadaEspecificaElement);
+        for (let i = 1; i <= 3; i++) {
+            const jornadaEspecificaElement = jornadaElement.querySelector(`Jornada${i}`);
 
-        partidos.forEach((partido, index) => {
-            const fotcaLocalElement = partido.querySelector('FotcaLocal');
-            const fotcaVisitanteElement = partido.querySelector('FotcaVisitante');
-            const nomEquipoLocalElement = partido.querySelector('NomEquipoLocal');
-            const nombreEquipoVisitanteElement = partido.querySelector('NombreEquipoVisitante');
-            const etiquetaPartidoElement = partido.querySelector('EtiquetaPartido');
-            const resultadoLocalElement = partido.querySelector('ResultadoLocal');
-            const resultadoVisitanteElement = partido.querySelector('ResultadoVIsitante');
+            if (jornadaEspecificaElement) {
+                const partidos = jornadaEspecificaElement.querySelectorAll(`Partido${i}`);
 
-            const fotcaLocal = document.getElementById(`fotcaLocal${index + 1}`);
-            const fotcaVisitante = document.getElementById(`fotcaVisitante${index + 1}`);
-            const nomEquipoLocal = document.getElementById(`nomEquipoLocal${index + 1}`);
-            const nombreEquipoVisitante = document.getElementById(`nombreEquipoVisitante${index + 1}`);
-            const etiquetaPartido = document.getElementById(`etiquetaPartido${index + 1}`);
-            const resultadoLocal = document.getElementById(`resultadoLocal${index + 1}`);
-            const resultadoVisitante = document.getElementById(`resultadoVisitante${index + 1}`);
+                partidos.forEach((partido, index) => {
+                    const fotcaLocalElement = partido.querySelector('fotcaLocal');
+                    const fotcaVisitanteElement = partido.querySelector('fotcaVisitante');
+                    const nomEquipoLocalElement = partido.querySelector('nomEquipoLocal');
+                    const nombreEquipoVisitanteElement = partido.querySelector('nombreEquipoVisitante');
+                    const etiquetaPartidoElement = partido.querySelector('etiquetaPartido');
+                    const resultadoLocalElement = partido.querySelector('resultadoLocal');
+                    const resultadoVisitanteElement = partido.querySelector('resultadoVisitante');
 
-            if (fotcaLocal && fotcaVisitante && nomEquipoLocal && nombreEquipoVisitante && etiquetaPartido && resultadoLocal && resultadoVisitante) {
-                fotcaLocal.src = fotcaLocalElement.textContent;
-                fotcaVisitante.src = fotcaVisitanteElement.textContent;
-                nomEquipoLocal.textContent = nomEquipoLocalElement.textContent;
-                nombreEquipoVisitante.textContent = nombreEquipoVisitanteElement.textContent;
-                etiquetaPartido.textContent = etiquetaPartidoElement.textContent;
-                resultadoLocal.textContent = resultadoLocalElement.textContent;
-                resultadoVisitante.textContent = resultadoVisitanteElement.textContent;
+                    const fotcaLocal = document.getElementById(`fotcaLocal${index + 1}`);
+                    const fotcaVisitante = document.getElementById(`fotcaVisitante${index + 1}`);
+                    const nomEquipoLocal = document.getElementById(`nomEquipoLocal${index + 1}`);
+                    const nombreEquipoVisitante = document.getElementById(`nombreEquipoVisitante${index + 1}`);
+                    const etiquetaPartido = document.getElementById(`etiquetaPartido${index + 1}`);
+                    const resultadoLocal = document.getElementById(`resultadoLocal${index + 1}`);
+                    const resultadoVisitante = document.getElementById(`resultadoVisitante${index + 1}`);
+
+                    if (fotcaLocal && fotcaVisitante && nomEquipoLocal && nombreEquipoVisitante && etiquetaPartido && resultadoLocal && resultadoVisitante) {
+                        fotcaLocal.src = fotcaLocalElement.textContent;
+                        fotcaVisitante.src = fotcaVisitanteElement.textContent;
+                        nomEquipoLocal.textContent = nomEquipoLocalElement.textContent;
+                        nombreEquipoVisitante.textContent = nombreEquipoVisitanteElement.textContent;
+                        etiquetaPartido.textContent = etiquetaPartidoElement.textContent;
+                        resultadoLocal.textContent = resultadoLocalElement.textContent;
+                        resultadoVisitante.textContent = resultadoVisitanteElement.textContent;
+                    }
+                });
+            } else {
+                console.log(`Неверный номер тура: ${i}`);
             }
-        });
+        }
     } else {
-        console.log("dinahu");
+        console.log("Неверный номер сезона");
     }
 }
