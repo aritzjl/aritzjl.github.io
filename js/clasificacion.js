035e23ad1371c8d8db4424e986bcad45ec129233
lamarireal <170219a7@gmail.com>
2024-02-07 12:51:44 +0100

    añadido calculador para tabla de clasificaciones

--- js/clasificacion.js
+++ js/clasificacion.js
@@ -1,24 +1,131 @@
 // Ruta del archivo XML
 const xmlFilePath = '/XMLyXSD/XMLClasificacion.xml';
 
-// Función para cargar un archivo XML
+// Функция для загрузки XML-документа
 function loadXMLDoc(filename) {
-    // Comprobamos si el navegador es compatible con XMLHttpRequest
     if (window.XMLHttpRequest) {
         var xhttp = new XMLHttpRequest();
     } else {
-        // En caso de ser Internet Explorer 6 o 5, utilizamos ActiveXObject
         var xhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
     
-    // Abrimos la conexión al archivo XML de manera síncrona (false)
     xhttp.open("GET", filename, false);
     xhttp.send();
     
-    // Devolvemos el documento XML
     return xhttp.responseXML;
 }
 
+// Функция для вычисления очков команды
+function calculatePoints(ganados, empatados) {
+    return (ganados * 3) + empatados;
+}
+
+function calculateJugados(ganados, perdidos, empatados) {
+    return ganados + perdidos + empatados;
+}
+
+// Функция для загрузки данных в таблицу из XML
+function loadTableData(selectedSeason) {
+    const xmlDoc = loadXMLDoc(xmlFilePath);
+
+    populateSeasonDropdown(xmlDoc);
+
+    const selectElement = document.getElementById('countries');
+
+    const temporadas = xmlDoc.getElementsByTagName('temporada');
+    let hasCurrentYear = false;
+    for (let i = 0; i < temporadas.length; i++) {
+        const temporada = temporadas[i];
+        const temporadaId = temporada.getAttribute('id');
+        if (temporadaId === '2024') {
+            hasCurrentYear = true;
+            break;
+        }
+    }
+
+    if (!hasCurrentYear) {
+        selectElement.innerHTML = '<option selected>Choose a Temporada</option>';
+        return;
+    }
+
+    selectElement.value = '2024';
+
+    const tableBody = document.getElementById('table-body');
+    tableBody.innerHTML = '';
+
+    const teams = xmlDoc.getElementsByTagName('team');
+    const teamsData = [];
+
+    for (let i = 0; i < teams.length; i++) {
+        const team = teams[i];
+        const temporadaId = team.closest('temporada').getAttribute('id');
+
+        if (temporadaId === selectedSeason) {
+            const position = team.getElementsByTagName('position')[0].textContent;
+            const escudoPath = team.getElementsByTagName('escudo_path')[0].textContent;
+            const nombre = team.getElementsByTagName('nombre')[0].textContent;
+            const ganados = parseInt(team.getElementsByTagName('ganados')[0].textContent);
+            const perdidos = parseInt(team.getElementsByTagName('perdidos')[0].textContent);
+            const empatados = parseInt(team.getElementsByTagName('empatados')[0].textContent);
+            const golesF = team.getElementsByTagName('goles_f')[0].textContent;
+            const golesC = team.getElementsByTagName('goles_c')[0].textContent;
+            const puntos = calculatePoints(ganados, empatados);
+            const jugados = calculateJugados(ganados, perdidos, empatados);
+
+            teamsData.push({
+                position: position,
+                escudoPath: escudoPath,
+                nombre: nombre,
+                ganados: ganados,
+                perdidos: perdidos,
+                empatados: empatados,
+                golesF: golesF,
+                golesC: golesC,
+                puntos: puntos,
+                jugados: jugados
+            });
+        }
+        // Сортируем команды по количеству очков и разнице в голах (по убыванию)
+teamsData.sort((a, b) => {
+    if (a.puntos !== b.puntos) {
+        return b.puntos - a.puntos; // Сортировка по количеству очков
+    } else {
+        // Если очки одинаковые, сравниваем разницу в голах
+        const diferenciaGolesA = a.golesF - a.golesC;
+        const diferenciaGolesB = b.golesF - b.golesC;
+        return diferenciaGolesB - diferenciaGolesA; // Учитываем разницу в голах
+    }
+});
+    }
+
+    // Сортируем команды по количеству очков (по убыванию)
+    teamsData.sort((a, b) => b.puntos - a.puntos);
+
+    // Добавляем отсортированные команды в таблицу
+    for (let i = 0; i < teamsData.length; i++) {
+        const teamData = teamsData[i];
+
+        const newRow = document.createElement('tr');
+        newRow.className = i % 2 === 0 ? ' dark:border-gray-700' : '';
+
+        newRow.innerHTML = `
+            <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">${i + 1}</td>
+            <td class="px-6 py-4"><img src="${teamData.escudoPath}" alt="Escudo Equipo" class="h-8"></td>
+            <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">${teamData.nombre}</td>
+            <td class="px-6 py-4">${teamData.ganados}</td>
+            <td class="px-6 py-4">${teamData.perdidos}</td>
+            <td class="px-6 py-4">${teamData.empatados}</td>
+            <td class="px-6 py-4">${teamData.jugados}</td>
+            <td class="px-6 py-4">${teamData.golesF}</td>
+            <td class="px-6 py-4">${teamData.golesC}</td>
+            <td class="px-6 py-4">${teamData.puntos}</td>
+        `;
+
+        tableBody.appendChild(newRow);
+    }
+}
+
+// Функция для загрузки выпадающего списка сезонов
 function populateSeasonDropdown(xmlDoc) {
     const selectElement = document.getElementById('countries');
     selectElement.innerHTML = '';
@@ -36,92 +143,13 @@ function populateSeasonDropdown(xmlDoc) {
     }
 }
 
-// Функция для загрузки динамических данных в таблицу из XML
-function loadTableData(selectedSeason) {
-    // Загружаем XML-документ
-    const xmlDoc = loadXMLDoc(xmlFilePath);
-
-    // Заполняем выпадающий список сезонов
-    populateSeasonDropdown(xmlDoc);
-
-    const selectElement = document.getElementById('countries');
-
-    // Проверяем, есть ли актуальный год (2024) в XML
-    const temporadas = xmlDoc.getElementsByTagName('temporada');
-    let hasCurrentYear = false;
-    for (let i = 0; i < temporadas.length; i++) {
-        const temporada = temporadas[i];
-        const temporadaId = temporada.getAttribute('id');
-        if (temporadaId === '2024') {
-            hasCurrentYear = true;
-            break;
-        }
-    }
-
-    if (!hasCurrentYear) {
-        // Если актуального года нет в XML, очищаем список выбора и завершаем функцию
-        selectElement.innerHTML = '<option selected>Choose a Temporada</option>';
-        return;
-    }
-
-    // Устанавливаем значение по умолчанию в выпадающем списке на "2024"
-    selectElement.value = '2024';
-
-    // Получаем тело таблицы по его ID
-    const tableBody = document.getElementById('table-body');
-    tableBody.innerHTML = '';
-
-    // Получаем все теги 'team' из XML
-    const teams = xmlDoc.getElementsByTagName('team');
-
-    // Итерируем по каждой команде в XML
-    for (let i = 0; i < teams.length; i++) {
-        const team = teams[i];
-        const temporadaId = team.closest('temporada').getAttribute('id');
-
-        if (temporadaId === selectedSeason) {
-            const position = team.getElementsByTagName('position')[0].textContent;
-            const escudoPath = team.getElementsByTagName('escudo_path')[0].textContent;
-            const nombre = team.getElementsByTagName('nombre')[0].textContent;
-            const ganados = team.getElementsByTagName('ganados')[0].textContent;
-            const perdidos = team.getElementsByTagName('perdidos')[0].textContent;
-            const empatados = team.getElementsByTagName('empatados')[0].textContent;
-            const jugados = team.getElementsByTagName('jugados')[0].textContent;
-            const golesF = team.getElementsByTagName('goles_f')[0].textContent;
-            const golesC = team.getElementsByTagName('goles_c')[0].textContent;
-            const puntos = team.getElementsByTagName('puntos')[0].textContent;
-
-            // Añadimos una clase alternante para el estilo de fila
-            const newRow = document.createElement('tr');
-            newRow.className = i % 2 === 0 ? ' dark:border-gray-700' : '';
-
-            // Insertamos el HTML en la nueva fila con los datos del equipo
-            newRow.innerHTML = `
-                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">${position}</td>
-                <td class="px-6 py-4"><img src="${escudoPath}" alt="Escudo Equipo" class="h-8"></td>
-                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">${nombre}</td>
-                <td class="px-6 py-4">${ganados}</td>
-                <td class="px-6 py-4">${perdidos}</td>
-                <td class="px-6 py-4">${empatados}</td>
-                <td class="px-6 py-4">${jugados}</td>
-                <td class="px-6 py-4">${golesF}</td>
-                <td class="px-6 py-4">${golesC}</td>
-                <td class="px-6 py-4">${puntos}</td>
-            `;
-
-            // Añadimos la nueva fila a la tabla
-            tableBody.appendChild(newRow);
-        }
-    }
-}
-
 // Функция loadTableData будет выполнена при полной загрузке окна
 window.onload = function() {
     const defaultSelectedSeason = '2024'; // Задаем 2024 год по умолчанию
     loadTableData(defaultSelectedSeason);
 };
 
-// La función loadTableData se ejecutará cuando la ventana haya cargado completamente
+// Функция loadTableData будет выполнена при изменении значения в выпадающем списке
 document.getElementById('countries').addEventListener('change', function() {
     const selectedSeason = this.value;
     loadTableData(selectedSeason);
